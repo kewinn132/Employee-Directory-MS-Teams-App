e51f62cfaa23fba22e8b004e724c2d2a79c989e3
kewinn132 <101753822+kewinn132@users.noreply.github.com>
2023-02-15 12:14:47 +0100

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 <body>
     <?php 
-        echo file_get_contents("http://www.example.com");
+        echo file_get_contents("https://aos-rts-sf-b1b468164ee-prod-northeurope.hr.talent.dynamics.com/namespaces/b43f9bd7-08ba-42e4-be45-75fcf3709c40/?cmp=apl&mi=HcmPeopleHub");
     ?>
 </body>
 
